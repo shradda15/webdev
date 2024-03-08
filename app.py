a3d8d3140fe46ada28624d32635ecea6efc7bf9e
shradda15 <129823516+shradda15@users.noreply.github.com>
2024-03-08 15:05:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+
 customer_orders = {
     "katrina kaif": ["chocolate", "sunglasses", "perfume","kite"]
 }
 
 app = FastAPI()
-class PromptInput(BaseModel):
+class add(BaseModel):
     customer_name: str
     products : List[str]
 
-@app.post("/chatgpt")
+@app.get("/add_order")
 def fn(customer_name : str):
     if customer_name in customer_orders:
         return customer_orders[customer_name]
     else:
-        return []   
+        return []  
 
-s=fn("katrina kaif")
-
-print(s)
+@app.post("/add_order")
+async def add_order(order_input: add):
+    customer_name = order_input.customer_name
+    products = order_input.products
+    customer_orders[customer_name] = products
