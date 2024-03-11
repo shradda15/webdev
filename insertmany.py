74d7548eaff584b462494c795f58393c40ce95b3
shradda15 <129823516+shradda15@users.noreply.github.com>
2024-03-11 20:59:42 +0530

    Create insertmany.py

---
+++ insertmany.py
@@ -0,0 +1,23 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+# Create a new client and connect to the server
+client = MongoClient("mongodb+srv://shradda:shradda@cluster0.mfvlisk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+# Send a ping to confirm a successful connection
+db=client["shopping"]
+col=db["items"]
+mylist = [
+  { "name": "cube", "price": 1999},
+  { "name": "kite", "price": 1199},
+  { "name": "sunglass", "price": 6000},
+  { "name": "perfume", "price": 1797},
+  {"name":"chocolates","price":5196},
+  {"name":"samsung","price":41999}
+]
+# col.insert_one({"name":"samsung","price":984982})
+# col.insert_one({"name":"chocolates","price":94234})
+# documents=list(col.find({"name":"chocolates"}))
+# print(documents[0]["price"])
+col.insert_many(mylist)
