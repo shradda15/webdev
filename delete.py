6e46b775c416b7143267d3869c282a008037cd9e
shradda15 <129823516+shradda15@users.noreply.github.com>
2024-03-12 00:05:40 +0530

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,26 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+# Create a new client and connect to the server
+client = MongoClient("mongodb+srv://shradda:shradda@cluster0.mfvlisk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+# Send a ping to confirm a successful connection
+db=client["shopping"]
+col=db["items"]
+# mylist = [
+#   { "name": "cube", "price": 1999},
+#   { "name": "kite", "price": 1199},
+#   { "name": "sunglass", "price": 6000},
+#   { "name": "perfume", "price": 1797},
+#   {"name":"chocolates","price":5196},
+#   {"name":"samsung","price":41999}
+# ]
+# documents=list(col.find().sort("price"))
+# print(documents[0]["price"])
+# print(documents)
+myquery = { "name": "cube" }
+col.delete_one(myquery)
+query = { "name": {"$regex": "^s"} }
+col.delete_many(query)
+col.delete_many({})
